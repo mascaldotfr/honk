8febf81b6eacff4c5c48a29ff52b1cd870bf1741
Ted Unangst <tedu@tedunangst.com>
2019-05-31 03:43:08 -0400

    faster and simpler cleanup by deleting everything older than 30 days

--- honk.go
+++ honk.go
@@ -1335,7 +1335,8 @@ func cleanupdb() {
 		}
 	}
 	expdate := time.Now().UTC().Add(-30 * 24 * time.Hour).Format(dbtimeformat)
-	doordie(db, "update files set content = '', local = 0 where length(content) > 20000 and fileid in (select fileid from donks join honks on donks.honkid = honks.honkid where honks.dt < ? and whofore = 0) and fileid not in (select fileid from donks join honks on donks.honkid = honks.honkid where whofore = 2 or whofore = 3)", expdate)
+	doordie(db, "delete from donks where honkid in (select honkid from honks where dt < ? and whofore = 0)", expdate)
+	doordie(db, "delete from honks where dt < ? and whofore = 0", expdate)
 	doordie(db, "delete from files where fileid not in (select fileid from donks)")
 }
 
