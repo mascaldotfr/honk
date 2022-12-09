22a1dbb022b070428758951ee711c12f79f0cb6a
mascal <nomail@example.org>
2022-12-09 20:23:43 +0100

    +Test script that tests honk @ localhost:9999
    --HG--
    branch : mascal

---
+++ tonk.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+testdir="/dev/shm"
+server="127.0.0.1:9999"
+
+make clean
+make
+make test || exit 1
+trap "cd ${testdir} && rm -rf honk view honk.db* blob.db*" 2 3
+cp -a honk views "${testdir}"
+cd "${testdir}"
+printf "mascal\nmascal\n${server}\n${server}\n" | ./honk init
+./honk
