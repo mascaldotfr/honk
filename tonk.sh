de1576674da6581696b492281799c20759b56e4c
mascal <nomail@example.org>
2022-12-12 16:53:37 +0100

    tonk.sh: add a "test" user, remove mentions of the gone "assetrs" directory
    --HG--
    branch : mascal

--- tonk.sh
+++ tonk.sh
@@ -6,8 +6,9 @@ server="127.0.0.1:9999"
 make clean
 make
 make test || exit 1
-trap "cd ${testdir} && rm -rf assets honk views honk.db* blob.db*" 2 3
+trap "cd ${testdir} && rm -rf honk views honk.db* blob.db*" 2 3
 cp -a honk views data/views "${testdir}"
 cd "${testdir}"
 printf "mascal\nmascal\n${server}\n${server}\n" | ./honk init
+printf "test\ntesttest\n" | ./honk adduser
 ./honk
