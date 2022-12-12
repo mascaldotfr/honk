69a1c90c843a18f0e60a4349cb06644c2ff88d4c
mascal <nomail@example.org>
2022-12-12 21:20:30 +0100

    PWA: stop spamming my console sw.js!
    --HG--
    branch : mascal

--- views/sw.js
+++ views/sw.js
@@ -1,4 +1,4 @@
 self.addEventListener('install', function (event) {
 	console.log('service worker installed')
 })
-self.addEventListener('fetch',() => console.log("fetch"));
+self.addEventListener('fetch',() => void 0)
